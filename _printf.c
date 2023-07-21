b39b609757384ccf4af7c9514baf0f58397254a9
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-21 15:04:43 +0100

    task1| printf.c: add handling conversion specifiers [d && i]

--- _printf.c
+++ _printf.c
@@ -39,7 +39,7 @@ int _printf(const char *format, ...)
 					putchar(specifier);
 					count++;
 					break;
-				case 's':
+				case 's':	
 					{
 						char *str = va_arg(args, char *);
 						while (*str)
@@ -49,6 +49,82 @@ int _printf(const char *format, ...)
 							count++;
 						}
 					}
+					break;
+				case 'i':
+					int n = va_arg(args,int);
+					int num, last = n%10, digit, exp =1;
+					int i = 1;
+					n = n/10;
+					
+					// if n is n= 5673, num would be n/10 
+					// last = n % 10
+					// since num =  n/10 = 567.[3] <- last
+					num = n;
+					
+					if (last < 0)
+					{
+						putchar('-');
+						num = -num;
+						n = -n;
+						last = -last;
+						count++;
+					}
+					if (num > 0)
+					{
+						while (num /10 != 0)
+						{
+							exp = exp * 10;
+							num = num / 10;
+						}
+						num = n;
+
+						while (exp > 0) 
+						{
+							digit = num / exp;
+							putchar(digit + '0');
+							exp = exp / 10;
+							count++;
+						}	
+					}
+					putchar(last + '0');
+					count++;
+					break;
+				case 'd':
+					int n = va_arg(args, int);
+					int num, last = n%10,digit;
+					int i = 1, exp = 1;
+
+					n = n/10;
+					num = n;
+					//handling negative sign
+					if (last < 0)
+					{
+						putchar('-');
+						num = -num;
+						n = -n;
+						last = -last;
+						count++;
+					}
+					if (num > 0)
+					{
+						while (num / 10 != 0)
+						{
+							exp = exp * 10;
+							num = num / 10;
+						}
+						num = n;
+						
+						while (exp > 0)
+						{
+							digit = num / exp;
+							putchar(digit + '0');
+							num = num - (digit * exp);
+							exp = exp / 10;
+							count++;
+						}
+					}
+					putchar(last + '0');
+					count++;
 					break;
 				default:
 					putchar('%');
