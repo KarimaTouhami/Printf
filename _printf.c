f0b805e524acf7163eacdfa8912b64957fcfaab2
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-21 16:39:24 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,19 +1,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-/**
- * _printf - produces output according to a format.
- * @format: format string containing the characters and the specifiers.
- *
- * Description: This function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt.
- *
- * Return: the number of characters printed (excluding the null byte used to
- * end output to strings).
- */
 int _printf(const char *format, ...)
 {
+	if (format == NULL)
+	{
+		/* Handle NULL format string error here */
+		return -1;
+	}
+
 	va_list args;
 	va_start(args, format);
 	int count = 0;
@@ -39,9 +34,11 @@ int _printf(const char *format, ...)
 					putchar(specifier);
 					count++;
 					break;
-				case 's':	
+				case 's':
 					{
 						char *str = va_arg(args, char *);
+						if (str == NULL)
+							str = "(null)";
 						while (*str)
 						{
 							putchar(*str);
@@ -50,86 +47,11 @@ int _printf(const char *format, ...)
 						}
 					}
 					break;
-				case 'i':
-					int n = va_arg(args,int);
-					int num, last = n%10, digit, exp =1;
-					int i = 1;
-					n = n/10;
-					
-					// if n is n= 5673, num would be n/10 
-					// last = n % 10
-					// since num =  n/10 = 567.[3] <- last
-					num = n;
-					
-					if (last < 0)
-					{
-						putchar('-');
-						num = -num;
-						n = -n;
-						last = -last;
-						count++;
-					}
-					if (num > 0)
-					{
-						while (num /10 != 0)
-						{
-							exp = exp * 10;
-							num = num / 10;
-						}
-						num = n;
-
-						while (exp > 0) 
-						{
-							digit = num / exp;
-							putchar(digit + '0');
-							exp = exp / 10;
-							count++;
-						}	
-					}
-					putchar(last + '0');
-					count++;
-					break;
-				case 'd':
-					int n = va_arg(args, int);
-					int num, last = n%10,digit;
-					int i = 1, exp = 1;
-
-					n = n/10;
-					num = n;
-					//handling negative sign
-					if (last < 0)
-					{
-						putchar('-');
-						num = -num;
-						n = -n;
-						last = -last;
-						count++;
-					}
-					if (num > 0)
-					{
-						while (num / 10 != 0)
-						{
-							exp = exp * 10;
-							num = num / 10;
-						}
-						num = n;
-						
-						while (exp > 0)
-						{
-							digit = num / exp;
-							putchar(digit + '0');
-							num = num - (digit * exp);
-							exp = exp / 10;
-							count++;
-						}
-					}
-					putchar(last + '0');
-					count++;
-					break;
 				default:
 					putchar('%');
 					putchar(*format);
 					count += 2;
+					break; /* Handle unknown specifier case */
 			}
 		}
 		else
