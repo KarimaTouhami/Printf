548c15c3eb41fd5e5a755193de5a2c23f016ea13
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-23 20:21:13 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,81 +1,72 @@
-#include <stdarg.h>
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * _printf - a function that produces output according to a format.
- * @format: character string.
- *
- * Return: the number of characters printed (excluding the null byte used to
- * end output to strings).
+ * is_alpha - checks if a character is alphabetic
+ * @c: the character to check
+ * Return: 1 if the character is alphabetic, 0 otherwise
+ */
+int is_alpha(char c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+static format_specifier format_specifiers[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{"%", print_percent},
+	{"d", print_integer},
+	{"i", print_integer},
+	{"b", print_binary},
+	{"o", print_octal},
+	{"x", print_hex},
+	{"X", print_hex},
+	{"u", print_unsigned},
+	{"p", print_address},
+	{"r", print_reverse},
+	{"R", print_rot13},
+	{"S", print_string_non_printable},
+	{NULL, NULL}
+};
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	if (format == NULL)
-	{
-		/* Handle NULL format string error here */
-		return -1;
-	}
-
+	int count = 0, i;
+	format_t f = {0, -1, -1, -1, -1};
 	va_list args;
-	va_start(args, format);
-	int count = 0;
-	char specifier;
 
+	/* Note: printf segfaults if format is NULL */
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			format++; /* Move past the '%' */
+			format++;
+			f = get_format(&format);
+			if (f.flags == NULL)
+				return (-1);
 
-			if (*format == NULL)
-			{
-				break;
-			}
-
-			switch (*format)
-			{
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				case 'c':
-					specifier = va_arg(args, int);
-					putchar(specifier);
-					count++;
-					break;
-				case 's':
+			for (i = 0; format_specifiers[i].specifier; ++i)
+				if (f.specifier == *format_specifiers[i].specifier)
 				{
-					char *str = va_arg(args, char *);
-					if (str == NULL)
-					{
-						str = "(null)";
-					}
-
-					while (*str)
-					{
-						putchar(*str);
-						str++;
-						count++;
-					}
-
+					format_specifiers[i].function(args, f, &count);
+					format++;
 					break;
 				}
-				default:
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break; /* Handle unknown specifier case */
-			}
+			free(f.flags);
 		}
 		else
-		{
-			putchar(*format);
-			count++;
-		}
-
-		format++;
+			_putchar(*format++, &count);
 	}
-
 	va_end(args);
 	return (count);
 }
