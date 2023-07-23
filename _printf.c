7bdadf06ff19097b12d573aab9bef2272b8a3772
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-23 20:50:13 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
  * _printf - Produces output according to a format.
  * @format: Character string.
@@ -21,33 +22,16 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				break;
 			if (*format == 'c')
-			{
-				putchar(va_arg(args, int));
-				count++;
-			}
+				putchar(va_arg(args, int)), count++;
 			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					putchar(*str++);
-					count++;
-				}
-			}
+				for (char *str = va_arg(args, char *); str && *str; putchar(*str++), count++)
+					;
 			else
-			{
-				putchar('%');
-				putchar(*format);
-				count += 2;
-			}
+				putchar('%'), putchar(*format), count += 2;
 		}
 		else
-		{
-			putchar(*format);
-			count++;
-		}
+			putchar(*format), count++;
+
 		format++;
 	}
 	va_end(args);
