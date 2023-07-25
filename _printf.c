346f8db4707aff5c89fb554185f6cec1d67f2abf
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-25 12:13:37 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,11 @@
 #include "main.h"
 
 /**
- * switch_char - helper function that handles format specifiers
- * @handle: va_list for variable arguments
- * @specifier: the format specifier character
- * Return: number of characters printed
- */
-int switch_char(va_list handle, char specifier)
-{
-	int result = 0;
-
-	switch (specifier)
-	{
-	case 'c':
-		_putchar(va_arg(handle, int));
-		result++;
-		break;
-	case 's':
-		result += switch_string(handle);
-		break;
-	case 'd':
-	case 'i':
-		result += switch_integer(handle);
-		break;
-	default:
-		_putchar('%');
-		_putchar(specifier);
-		result += 2;
-		break;
-	}
-
-	return result;
-}
-
-/**
  * _printf - function that produces output according to a format
  * @format: character string
  * Return: counter of char
  */
+
 int _printf(const char *format, ...)
 {
 	int result = 0;
@@ -45,16 +13,9 @@ int _printf(const char *format, ...)
 
 	va_start(handle, format);
 	if (!format || (format[0] == '%' && !format[1]))
-	{
-		va_end(handle);
 		return (-1);
-	}
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	{
-		va_end(handle);
 		return (-1);
-	}
-
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -69,7 +30,6 @@ int _printf(const char *format, ...)
 		}
 		format++;
 	}
-
 	va_end(handle);
 	return (result);
 }
