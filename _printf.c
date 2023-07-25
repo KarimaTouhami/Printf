6351af7e2f0a7de63c77ed8a83acaa27967ddda8
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-25 19:49:10 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -8,28 +8,28 @@
 
 int _printf(const char *format, ...)
 {
-	int result = 0;
-	va_list handle;
+    int result = 0;
+    va_list args;
 
-	va_start(handle, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			result += switch_char(handle, *format);
-		}
-		else
-		{
-			_putchar(*format);
-			result++;
-		}
-		format++;
-	}
-	va_end(handle);
-	return (result);
+    va_start(args, format);
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            result += switch_char(args, *format);
+        }
+        else
+        {
+            _putchar(*format);
+            result++;
+        }
+        format++;
+    }
+    va_end(args);
+    return (result);
 }
