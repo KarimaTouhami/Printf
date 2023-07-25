f4c73736db0d4403e981dfec8764e42fec06bd46
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-25 21:45:18 +0100

    NEW

--- main.h
+++ main.h
@@ -3,18 +3,11 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
-#include <stdint.h>
-
-#define OUTPUT_SIZE 1024
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int printf_char(va_list handle); // Prototype for the function used in switch_char
-int printf_string(va_list handle); // Prototype for the function used in switch_char
-int switch_char(va_list handle, char cara); // Prototype for the helper function in _printf
-int printf_int(va_list handle); // Prototype for the function used in switch_char
+int printf_char(va_list args);
+int printf_string(va_list args);
 
 #endif
