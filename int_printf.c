e236cce62a83c244c58fa463dc20a523d3d03905
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-25 23:50:16 +0100

    add decimal fomart

--- int_printf.c
+++ int_printf.c
@@ -47,3 +47,51 @@ int printf_int(va_list handle)
 
 	return (count);
 }
+
+/**
+ * printf_dec - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+
+int printf_dec(va_list handle)
+{
+	int n = va_arg(args, int);
+	int num, last = n%10,digit;
+	int count = 1, exp = 1;
+
+	n = n/10;
+	num = n;
+	
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		count++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+			
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			count++;
+		}
+	}
+	_putchar(last + '0');
+	count++;
+	
+	return (count);
+}
+
