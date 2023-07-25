f8db57b7d054fa8f9e04bc7d19838eebf3808850
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-25 19:59:15 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 /**
  * _printf - function that produces output according to a format
  * @format: character string
- * Return: counter of char
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
  */
 
 int _printf(const char *format, ...)
