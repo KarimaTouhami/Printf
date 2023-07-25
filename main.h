cef280a8700181dc0fd36a447c7e4d259e6ecbc6
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-25 21:55:18 +0100

    Update main.h

--- main.h
+++ main.h
@@ -3,11 +3,18 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdint.h>
+
+#define OUTPUT_SIZE 1024
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int printf_char(va_list args);
-int printf_string(va_list args);
+int printf_char(va_list handle);
+int printf_string(va_list handle);
+int switch_char(va_list handle, char cara);
+int printf_int(va_list handle);
 
 #endif
