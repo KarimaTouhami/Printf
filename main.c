782d2431a8faec59b8554320181c7878644718e3
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-26 21:11:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,21 +8,15 @@
 int main(void)
 {
     int test = 0;
-
     test = _printf("test char %d\n", 0);
     printf("%d\n", test);
-
     test = _printf("test char %d\n", 12);
     printf("%d\n", test);
-
     test = _printf("test char %d\n", 34535354);
     printf("%d\n", test);
-
     test = _printf("test char %d\n", -34535354);
     printf("%d\n", test);
-
     test = _printf("test char %d\n", 2147483647);
     printf("%d\n", test);
-
     return (0);
 }
