ec8e55759e157dbc72ef6c0a50eaa5fb18c94056
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-26 21:25:13 +0100

    add %d%i spec to _printf

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
 
 int _printf(const char *format, ...)
 {
-	convert_match m[] = {{"%s", printf_string}, {"%c", printf_char},{"%%", printf_37}};
+	convert_match m[] = {{"%s", printf_string}, {"%c", printf_char},{"%%", printf_37},{"%i", printf_int}, {"%d", printf_dec}};
 
 	va_list args;
 	int i = 0, j, len = 0;
