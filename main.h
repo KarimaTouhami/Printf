98c3d4c1aaaeb17b3080b21857b489369b6736aa
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-26 20:49:51 +0100

    add str helpers headers

--- main.h
+++ main.h
@@ -28,5 +28,7 @@ int printf_char(va_list val);
 int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _strlen(char *s);
+int _strlenc(const char *s);
 
 #endif
