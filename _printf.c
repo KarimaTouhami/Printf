6a368e960b010c9c748eb98bc78dfa40e50247c0
ayoubelmohamedi <ayoubprog61@gmail.com>
2023-07-26 21:20:09 +0100

    add string std headers

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 2;
+		j = 4;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
