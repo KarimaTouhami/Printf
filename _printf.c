a9bcbc8779e3107650f91583a642266a85e28c10
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-28 22:09:38 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,69 +1,49 @@
 #include "main.h"
 
 /**
- * _printf - Function that produces output according to a format.
+ * _printf - function that produces output according to a format
  * AUTHORS: TouhamiKarima & Ayoub Elmohamedi
- * @format: Format string containing the specifiers.
- *
- * Return: The number of characters printed (excluding the null byte
- * used to end output to strings).
+ * @format: identifier
+ * Return: string length
  */
-
 int _printf(const char *format, ...)
 {
 	convert_match m[] = {
-		{"%s", printf_string},
-		{"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int},
-		{"%d", printf_dec},
-		{"%b", printf_bin}, // Assuming you have implemented this function.
-		{"%u", printf_unsigned},
-		{"%o", printf_oct},
-		{"%x", printf_hex},
-		{"%X", printf_HEX},
-		{"%S", printf_exclusive_string},
-		{"%p", printf_pointer}
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37}, {"%i", printf_int},
+		{"%d", printf_dec}, {"%b", printf_bin},
+		{"%u", printf_unsigned}, {"%o", printf_oct},
+		{"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
 	};
 
 	va_list args;
-	int i, j, len = 0;
-	int num_convert_match = sizeof(m) / sizeof(m[0]);
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
-
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-	{
-		va_end(args);
 		return (-1);
-	}
 
-	for (i = 0; format[i] != '\0'; i++)
+	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		j = 11;
+		while (j >= 0)
 		{
-			for (j = 0; j < num_convert_match; j++)
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				if (m[j].id[0] == '%' && m[j].id[1] == format[i + 1])
-				{
-					len += m[j].f(args);
-					i++;
-					break;
-				}
+				len += m[j].f(args);
+				i = i + 2;
+				break;
 			}
-			if (j == num_convert_match)
-			{
-				_putchar('%');
-				len++;
-			}
+			j--;
 		}
-		else
+		if (j < 0)
 		{
 			_putchar(format[i]);
 			len++;
+			i++;
 		}
 	}
-
 	va_end(args);
 	return (len);
 }
