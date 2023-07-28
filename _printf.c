832dffe269e8bf4e720bac6c5343fffe08425102
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-28 20:05:23 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,39 +10,59 @@
 int _printf(const char *format, ...)
 {
 	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37}, {"%i", printf_int},
-		{"%d", printf_dec},{"%b",printf_bin},
-		{"%u", printf_unsigned},{"%o", printf_oct},
-		{"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string},{"%p", printf_pointer}
+		{"%s", printf_string},
+		{"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int},
+		{"%d", printf_dec},
+		{"%b", printf_bin}, // Assuming you have implemented this function.
+		{"%u", printf_unsigned},
+		{"%o", printf_oct},
+		{"%x", printf_hex},
+		{"%X", printf_HEX},
+		{"%S", printf_exclusive_string},
+		{"%p", printf_pointer}
 	};
 
 	va_list args;
-	int i = 0, j, len = 0;
+	int i, len = 0;
+	int num_convert_match = sizeof(m) / sizeof(m[0]);
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-Here:
-	while (format[i] != '\0')
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 	{
-		j = 11;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		va_end(args);
+		return (-1);
 	}
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			int j;
+			for (j = 0; j < num_convert_match; j++)
+			{
+				if (m[j].id[0] == '%' && m[j].id[1] == format[i + 1])
+				{
+					len += m[j].f(args);
+					i++;
+					break;
+				}
+			}
+			if (j == num_convert_match)
+			{
+				_putchar('%');
+				len++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+	}
+
 	va_end(args);
 	return (len);
 }
