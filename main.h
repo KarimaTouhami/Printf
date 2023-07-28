e2cc0389d51f605d996e210dff1a1f04f9f35c06
KarimaTouhami <113696378+KarimaTouhami@users.noreply.github.com>
2023-07-28 21:58:10 +0100

    Update main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <limits.h>
 #include <unistd.h>
 
-
+#define OUTPUT_SIZE 1024
 
 /**
  * struct format - match the conversion specifiers for printf
@@ -18,26 +18,19 @@
 
 typedef struct format
 {
-    char *id;
-    int (*f)();
-    int precision;
+	char *id;
+	int (*f)();
 } convert_match;
 
 int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
 int printf_HEX_aux(unsigned int num);
-<<<<<<< HEAD
 int printf_hex_aux(unsigned long int num);
-=======
->>>>>>> 86bec82c9f446e84dc9370fd333cf2a62e5ea101
 int printf_exclusive_string(va_list val);
 int printf_HEX(va_list val);
 int printf_hex(va_list val);
 int printf_oct(va_list val);
 int printf_unsigned(va_list args);
 int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
 int printf_int(va_list args);
 int printf_dec(va_list args);
 int _strlen(char *s);
@@ -51,4 +44,3 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
-
